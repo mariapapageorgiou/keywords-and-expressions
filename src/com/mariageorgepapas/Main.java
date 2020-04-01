1cd3907b5698c2fe7812f54d74d6bca02c89e415
morpheus <mariageorgepapas@gmail.com>
2020-03-31 23:17:56 -0500

    Keywords and Expressions

---
+++ src/com/mariageorgepapas/Main.java
@@ -0,0 +1,36 @@
+package com.mariageorgepapas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);  // double is the data type,
+                                               // "kilometers = (100 * 1.609344)" is the expression not including the semicolon
+
+        int highScore = 50;   // same here, int is the data type, "highScore = 50" is the expression, not icluding the semicolon
+
+        if(highScore == 50) {   // "highScore == 50" is the expression
+            System.out.println("This is an expression");
+        }
+
+
+
+
+
+        // In the following code, what parts of the code are expressions?
+        int score = 100;                                        // score = 100
+        if (score > 99) {                                       // score > 99
+            System.out.println("You got the high score!");      // "You got the high score"
+            score = 0;                                          // score = 0
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
